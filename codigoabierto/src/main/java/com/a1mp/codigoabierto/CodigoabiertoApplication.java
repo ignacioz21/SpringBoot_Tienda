2259a9ebe8f34d8164832d5d31bb352ebf54e29a
ignacioz21 <168688549+ignacioz21@users.noreply.github.com>
2025-07-28 10:23:10 -0600

    First build SpringBoot project

---
+++ codigoabierto/src/main/java/com/a1mp/codigoabierto/CodigoabiertoApplication.java
@@ -0,0 +1,13 @@
+package com.a1mp.codigoabierto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoabiertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigoabiertoApplication.class, args);
+	}
+
+}
---
+++ codigoabierto/src/test/java/com/a1mp/codigoabierto/CodigoabiertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a1mp.codigoabierto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigoabiertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
